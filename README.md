ef37bedd3302bb74257a0ae4e2fbf31fe9c69cae
jauhoja <67386247+jauhoja@users.noreply.github.com>
2024-02-03 00:48:39 +0200

    clean any dirt

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # ug-formatter
-Format chord sheets in Ultimate Guitar format to more pdf friendly format.
-
-Project uses PyEnchant thus install it. https://pypi.org/project/pyenchant/
-$ pip install pyenchant
-
-
-English words from https://github.com/dwyl/english-words/tree/master
-Unlicense license
-
-Suomisanat täältä: https://github.com/hugovk/everyfinnishword/tree/master
-LGPL-2.1 license
+Format chord sheets in Ultimate Guitar format to more pdf friendly format. Copy and paste chords from UG to input.txt and run script and output.txt will be written containing formatted sheet. Copy and paste to i.e. docs and print out pdf.
--- format-ug-nice.py
+++ format-ug-nice.py
@@ -1,8 +1,7 @@
-import os
-import itertools
+import re
 
-UG_HEADINGS = ["[Intro]", "[Verse]", "[Pre-chorus]", "[Chorus]", "[Interlude]", "[Bridge]", "[Solo]", "[Outro]"] 
-ENGLISH_WORDS = []
+UG_HEADINGS = ["[Intro]", "[Verse]", "[Verse 1]", "[Verse 2]", "[Verse 3]", "[Verse 4]", "[Verse 5]"] + \
+              ["[Pre-chorus]", "[Chorus]", "[Interlude]", "[Bridge]", "[Break]", "[Solo]", "[Outro]"] 
 CHORD_ROOTS = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'H', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'h'] + \
               ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#', 'H#', 'c#', 'd#', 'e#', 'f#', 'g#', 'a#', 'b#', 'h#'] + \
               ['Cb', 'Db', 'Eb', 'Fb', 'Gb', 'Ab', 'Bb', 'Hb', 'cb', 'db', 'eb', 'fb', 'gb', 'ab', 'bb', 'hb'] + \
@@ -10,15 +9,14 @@ CHORD_ROOTS = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'H', 'c', 'd', 'e', 'f', 'g', 
               ['Ces', 'Des', 'Ees', 'Fes', 'Ges', 'Aes', 'Bes', 'Hes', 'ces', 'des', 'ees', 'fes', 'ges', 'aes', 'bes', 'hes']
 CHORD_SYMBOLS = []
 
-
-
 def initChordSymbols():
   file = open("./chord-qualities.txt", 'r')
   chordQualities = file.read().splitlines()
-  
+  file.close()
   for root in CHORD_ROOTS:
     for quality in chordQualities:
       CHORD_SYMBOLS.append(root + quality)
+  CHORD_SYMBOLS.append('N.C.') # No chord, any missing symbols can be added here
 
 def rowContainsAnyChords(words : [str]):
   for word in words:
@@ -26,55 +24,34 @@ def rowContainsAnyChords(words : [str]):
       return True
   return False
 
-
-def rowContainsAnyWords(words : [str], wordlist : [str]):
-  for word in words:
-    if word in ENGLISH_WORDS:
-      return True
-  return False
-
 def rowContainsHeading(words : [str]):
   for word in words:
     if word in UG_HEADINGS:
+      return True
+    if '[' in word or ']' in word:
       return True
   return False
 
 def main (filename : str):
   initChordSymbols()
-  file = open("./words.txt", 'r')
-  ENGLISH_WORDS = file.read().splitlines()
-  file.close()
-
-  doesRowContainWords = []
 
   file = open(filename, 'r')
   rows = file.read().splitlines()
   file.close()
 
-  resultFile = open("./result.txt", 'w')
+  resultFile = open("./output.txt", 'w')
 
   for row in rows:
     if len(row) == 0 or len(row) == 1 and row[0] == ' ': continue
-    words = row.split(' ')
+
+    words = re.split("[ \t]", row)
+    if rowContainsHeading(words):
+      resultFile.write("\n" + '**' + row + '**' + "\n\n")
+      continue
     if rowContainsAnyChords(words):
       resultFile.write(row + "\n")
       continue
 
     resultFile.write(row + "\n\n")
-    """
-    if rowContainsHeading(words):
-      resultFile.write(row + "\n\n")
-      continue
-      
-    if rowContainsAnyWords(words):
-      resultFile.wirte(row + "\n")
 
-    doesRowContainWords.append(rowContainsAWord)
-    """
-
-
-
-  
-  return 0
-
-main('./lockedoutofheaven.txt')
+main('./input.txt')
