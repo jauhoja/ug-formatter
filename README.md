e57822305ed234b8243871ff742639ba9bbe968f
jauhoja <67386247+jauhoja@users.noreply.github.com>
2024-02-02 21:39:33 +0200

    wip

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ug-formatter
 Format chord sheets in Ultimate Guitar format to more pdf friendly format.
+
+Project uses PyEnchant thus install it. https://pypi.org/project/pyenchant/
+$ pip install pyenchant
+
+
+English words from https://github.com/dwyl/english-words/tree/master
+Unlicense license
+
+Suomisanat täältä: https://github.com/hugovk/everyfinnishword/tree/master
+LGPL-2.1 license
--- format-ug-nice.py
+++ format-ug-nice.py
@@ -1,15 +1,48 @@
-import enchant
 import os
-
+import itertools
 
 UG_HEADINGS = ["[Intro]", "[Verse]", "[Pre-chorus]", "[Chorus]", "[Interlude]", "[Bridge]", "[Solo]", "[Outro]"] 
+ENGLISH_WORDS = []
+CHORD_ROOTS = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'H', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'h'] + \
+              ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#', 'H#', 'c#', 'd#', 'e#', 'f#', 'g#', 'a#', 'b#', 'h#'] + \
+              ['Cb', 'Db', 'Eb', 'Fb', 'Gb', 'Ab', 'Bb', 'Hb', 'cb', 'db', 'eb', 'fb', 'gb', 'ab', 'bb', 'hb'] + \
+              ['Cis', 'Dis', 'Eis', 'Fis', 'Gis', 'Ais', 'Bis', 'His', 'cis', 'dis', 'eis', 'fis', 'gis', 'ais', 'bis', 'his'] + \
+              ['Ces', 'Des', 'Ees', 'Fes', 'Ges', 'Aes', 'Bes', 'Hes', 'ces', 'des', 'ees', 'fes', 'ges', 'aes', 'bes', 'hes']
+CHORD_SYMBOLS = []
 
-def wordExistsInLanguage(word : str, wordlist : [str]):
-  return 0
+
+
+def initChordSymbols():
+  file = open("./chord-qualities.txt", 'r')
+  chordQualities = file.read().splitlines()
+  
+  for root in CHORD_ROOTS:
+    for quality in chordQualities:
+      CHORD_SYMBOLS.append(root + quality)
+
+def rowContainsAnyChords(words : [str]):
+  for word in words:
+    if word in CHORD_SYMBOLS:
+      return True
+  return False
+
+
+def rowContainsAnyWords(words : [str], wordlist : [str]):
+  for word in words:
+    if word in ENGLISH_WORDS:
+      return True
+  return False
+
+def rowContainsHeading(words : [str]):
+  for word in words:
+    if word in UG_HEADINGS:
+      return True
+  return False
 
 def main (filename : str):
+  initChordSymbols()
   file = open("./words.txt", 'r')
-  englishWords = file.read().splitlines()
+  ENGLISH_WORDS = file.read().splitlines()
   file.close()
 
   doesRowContainWords = []
@@ -18,21 +51,29 @@ def main (filename : str):
   rows = file.read().splitlines()
   file.close()
 
-  for row in rows:
-    rowContainsAWord = False
-    words = row.split(' ')
-    for word in words:
-      if word in englishWords:
-        rowContainsAWord = True
-        break
-    doesRowContainWords.append(rowContainsAWord)
-  
+  resultFile = open("./result.txt", 'w')
 
-  """ for debugging
-  file = open("./result.txt", 'w')
-  for i in range(len(rows)):
-    file.write("[" + str(doesRowContainWords[i]) + "] " + rows[i] + "\n")
-  """
+  for row in rows:
+    if len(row) == 0 or len(row) == 1 and row[0] == ' ': continue
+    words = row.split(' ')
+    if rowContainsAnyChords(words):
+      resultFile.write(row + "\n")
+      continue
+
+    resultFile.write(row + "\n\n")
+    """
+    if rowContainsHeading(words):
+      resultFile.write(row + "\n\n")
+      continue
+      
+    if rowContainsAnyWords(words):
+      resultFile.wirte(row + "\n")
+
+    doesRowContainWords.append(rowContainsAWord)
+    """
+
+
+
   
   return 0
 
